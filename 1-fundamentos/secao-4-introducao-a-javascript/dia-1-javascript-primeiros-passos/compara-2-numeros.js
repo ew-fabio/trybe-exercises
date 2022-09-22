fe772195e8ab8210b6c59a6dd5027b43fa10795f
ew-fabio <juba.sk8@gmail.com>
2022-09-22 02:18:26 -0300

    finaliza o exercício de comparação com 2 e 3 números

---
+++ 1-fundamentos/secao-4-introducao-a-javascript/dia-1-javascript-primeiros-passos/compara-2-numeros.js
@@ -0,0 +1,12 @@
+//constantes do teste
+const num1 = 100;
+const num2 = num1 / 2;
+
+//condição de comparação
+if (num1 > num2) {
+	console.log("num1 é maior");
+} else if (num1 < num2) {
+	console.log("num2 é maior");
+} else {
+	console.log("são valores iguais");
+}
