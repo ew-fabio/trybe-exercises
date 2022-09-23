2de473fd4b646c93584ffa8896284709194f1089
ew-fabio <juba.sk8@gmail.com>
2022-09-23 02:14:30 -0300

    finaliza o exercício: isere comentários

--- 1-fundamentos/secao-4-introducao-a-javascript/dia-1-javascript-primeiros-passos/calcula-salario.js
+++ 1-fundamentos/secao-4-introducao-a-javascript/dia-1-javascript-primeiros-passos/calcula-salario.js
@@ -1,32 +1,55 @@
-let salarioBruto = Number(prompt('Digite seu salário bruto: ')); //entrada do usuário
-let salarioBase = salarioBruto - aliquotaINSS; //variável de cálculo do salário-base
-let descontoIR = (salarioBase * aliquotaIR) - deducao; //variável de cálculo do desconto de IR
+//início do algoritmo
+let salarioBruto = Number(prompt("Digite seu salário bruto: ")); //entrada do usuário
 
-let salarioLiquido = salarioBase - descontoIR; //reultado final: salário-líquido
-
-//variáveis
-let aliquotaINSS = 0;
-let aliquotaIR = 0;
-let deducao = 0;
-let salarioMinimo = 1212.00; //salário mínimo em 2022
+//salário mínimo em 2022
+let salarioMinimo = 1212.0;
 
 //índice - INSS
-let indiceA = salarioBruto - (salarioBruto * 0.08);
-let indiceB = salarioBruto - (salarioBruto * 0.09);
-let indiceC = salarioBruto - (salarioBruto * 0.11);
-let indiceD = salarioBruto - 570.88;
+let indiceA = salarioBruto * 0.08;
+let indiceB = salarioBruto * 0.09;
+let indiceC = salarioBruto * 0.11;
+let indiceD = 570.88;
 
 //alíquota - INSS
-if (salarioBruto >= salarioMinimo || salarioBruto <= 1556.94) {
-  aliquotaINSS = indiceA;
-} else if (salarioBruto >= 1556.95 || salarioBruto <= 2594.92) {
-  aliquotaINSS = indiceB;
-} else if (salarioBruto >= 2594.93 || salarioBruto <= 5189.82) {
-  aliquotaINSS = indiceC;
+let aliquotaINSS = 0;
+
+if (salarioBruto >= salarioMinimo && salarioBruto <= 1556.94) {
+	aliquotaINSS = indiceA;
+} else if (salarioBruto >= 1556.95 && salarioBruto <= 2594.92) {
+	aliquotaINSS = indiceB;
+} else if (salarioBruto >= 2594.93 && salarioBruto <= 5189.82) {
+	aliquotaINSS = indiceC;
 } else if (salarioBruto > 5189.82) {
-  aliquotaINSS = indiceD;
+	aliquotaINSS = indiceD;
 } else {
-  alert('Salário menor que o mínimo permitido para cálculo!')
+	alert("Salário menor que o mínimo permitido para cálculo!");
 }
 
-//
+//salário-base
+let salarioBase = salarioBruto - aliquotaINSS;
+
+//alíquota - IR
+let aliquotaIR = 0;
+let deducao = 0;
+
+if (salarioBase >= 1903.98 && salarioBase <= 2826.65) {
+	aliquotaIR = 0.075;
+	deducao = 142.8;
+} else if (salarioBase >= 2826.66 && salarioBase <= 3751.05) {
+	aliquotaIR = 0.15;
+	deducao = 354.8;
+} else if (salarioBase >= 3751.06 && salarioBase <= 4664.68) {
+	aliquotaIR = 0.225;
+	deducao = 636.13;
+} else if (salarioBase > 4668.68) {
+	aliquotaIR = 0.275;
+	deducao = 869.36;
+}
+
+//desconto - IR
+let descontoIR = salarioBase * aliquotaIR - deducao; //variável de cálculo do desconto de IR
+
+//salário líquido
+let salarioLiquido = salarioBase - descontoIR;
+
+alert("Salário líquido: " + salarioLiquido); //exibe o resultado para o usuário
