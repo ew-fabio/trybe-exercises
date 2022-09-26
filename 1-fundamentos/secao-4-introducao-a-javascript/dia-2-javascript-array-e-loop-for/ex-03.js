69a6e4c7c2ccfc9425385f2bb2fcf6c383a72c2c
ew-fabio <juba.sk8@gmail.com>
2022-09-26 03:30:03 -0300

    exercício 3 finalizado

--- 1-fundamentos/secao-4-introducao-a-javascript/dia-2-javascript-array-e-loop-for/ex-03.js
+++ 1-fundamentos/secao-4-introducao-a-javascript/dia-2-javascript-array-e-loop-for/ex-03.js
@@ -1,11 +1,14 @@
 //variável padrão
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-//ex-02 - Some todos os valores contidos no array e imprima o resultado;
+//ex-03 - Calcule e imprima a média aritmética dos valores contidos no array;
 let numbersSum = 0;
+let numbersAverage = 0;
 
 for (let i = 0; i < numbers.length; i = i + 1) {
 	numbersSum = numbersSum + numbers[i];
 }
 
-console.log(numbersSum);
+numbersAverage = numbersSum / numbers.length;
+
+console.log(numbersAverage);
