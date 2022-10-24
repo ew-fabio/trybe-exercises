867b88d0590d522578cb457f5b3576162ce362c6
ew-fabio <juba.sk8@gmail.com>
2022-10-23 22:35:34 -0300

    exercício 5 concluído

---
+++ 1-fundamentos/secao-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em -jest/ex-5.js
@@ -0,0 +1,23 @@
+const lista = ['React', 'Jest', 'HTML', 'CSS', 'JavaScript'];
+
+const techList = (lista, nome) => {
+  try {
+  if (lista.length === 0) {
+    throw new Error('Vazio!');
+  }
+  } catch (e) {
+    return e.message;
+  }
+  const listaObjetos = [];
+  lista.sort();
+    for (let i = 0; i < lista.length; i += 1) {
+      listaObjetos.push({tech: lista[i], name: nome});
+    }
+  return listaObjetos;
+
+
+};
+
+//console.log(techList(lista, 'Lucas'));
+
+module.exports = techList;
