6effb8e613d37f3a047ef36bbf840c623845adeb
Fabio Sampaio <106818866+ew-fabio@users.noreply.github.com>
2023-03-14 12:09:43 -0300

    Delete 2-front-end/secao-7-gerenciamento-de-estado-com-redux/dia-2-usando-o-redux-no-react directory

--- 2-front-end/secao-7-gerenciamento-de-estado-com-redux/dia-2-usando-o-redux-no-react/react-redux-test/src/redux/actions/index.js
+++
@@ -1,8 +0,0 @@
-export const actionCreator = (increment = 1) => ({
-  type: 'INCREMENT_COUNTER',
-  payload: increment,
-});
-
-// export const clickCounter = () => ({
-//   type: 'INCREMENT_CLICK',
-// });
